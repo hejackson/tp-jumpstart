6ca206f4281b09061dc1b42b028ccd823bafee29
Ed Jackson <edjacks+github@gmail.com>
2016-12-18 16:20:16 -0500

    add 01 Hello World program

---
+++ 01_hello-world/01_hello-world.py
@@ -0,0 +1,10 @@
+#!/usr/bin python3
+
+print('-------------')
+print('Hello App')
+print('-------------')
+
+user_text= input('What is your name? ')
+greeting = 'Nice to meet you ' + user_text
+print(greeting)
+
